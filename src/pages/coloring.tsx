932404f99036d70ae23222fb6d7b6bde79e43096
miguel <miguelsoldado12@gmail.com>
2024-05-01 23:18:54 +0100

    add a vercel.json

--- src/pages/coloring.tsx
+++ src/pages/coloring.tsx
@@ -1,13 +1,13 @@
 import { AppProvider } from "../provider";
-import { generateRandomArt } from "../../utils";
+// import { generateRandomArt } from "../../utils";
 import { Grid, BottomMenu, SideBar } from "../components/coloring";
 import mario from "../mario.json";
 
-const width = 16 * 2;
-const height = 9 * 2;
-const colorAmount = 10;
+// const width = 16 * 2;
+// const height = 9 * 2;
+// const colorAmount = 10;
 
-const state = generateRandomArt({ width, height, colorAmount });
+// const state = generateRandomArt({ width, height, colorAmount });
 
 export function Coloring() {
   return (
