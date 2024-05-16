d1fe3dbaff66011a12dbd9627d55968d54d6d583
miguel <miguelsoldado12@gmail.com>
2024-05-16 17:17:19 +0100

    .

--- src/pages/coloring.tsx
+++ src/pages/coloring.tsx
@@ -6,7 +6,6 @@ import { Grid, BottomMenu, SideBar } from "@/components/coloring";
 export function Coloring() {
   const navigate = useNavigate();
   const params = useParams();
-
   const item = content.find((item) => item.path === params.path);
 
   if (item === undefined) {
